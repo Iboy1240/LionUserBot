ae8f63f514c7d36ae57ad2776bef75100ee75bb1
Zi King <65592198+Zi-King@users.noreply.github.com>
2020-06-12 12:48:21 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,19 +13,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY STATUS** \n`FRIDAY IS:` **✅ Alive**\n\n"
-                     "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-                     "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE 🙂**\n`NO PROBLEM FOUND🔥!\n`"
-                     "`CONNECTION PROVIDER:` ** AMAZON WEB SERVICES\n"
-                     "`CURRECT BOT LOCATION:` ** ASHBURN,UNITED STATES\n"
-                     "`DEPLOYING SERVICE BY: HEROKU INC.\n"
-                     "`IP:` 3.82.66.232\n"
-                     "`SYSTEM TYPE:` LINUX\n"
-                     "`BASED ON:` UNIBORG\n"
-                     "`SATELLITE:` ⚡STARK SAT-2⚡\n"
-                     "`MADE USING:` PYTHON\n"
-                     f"`MY BOSS`: {DEFAULTUSER}\n\n"
-                     "ALWAYS WITH YOU MY BOSS\n\n"
-                     "`SATELLITE STATUS: ✅ Alive\n\n"
-                     "[⚡DEPLOY FRIDAY⚡](https://github.com/StarkGang/FRIDAYUserbot)") 
+    await alive.edit("**Lion UserBot is up and runjing! *\n\n"
+                     "Neutron: 6.0.9 \nProton: 3.7.4\n"
+                     "Heroku: Connected "
+                     "OS: GNU/Linux\n"
+                     "UserBot location: USA
+                     ") 
 
